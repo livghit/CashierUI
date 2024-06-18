91bab20623104e1d359839e13924af0155da9f71
livghit <liviu.g01@gmail.com>
2024-06-18 16:07:31 +0200

    wip

--- resources/views/billing.blade.php
+++ resources/views/billing.blade.php
@@ -9,12 +9,16 @@
     <title>Billing Management</title>
     @livewireScripts
     @livewireStyles
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <div>
-        <livewire:cashierui.test />
-        <p>billing portal </p>
+    <div class="grid grid-cols-6">
+        <div class="hidden md:col-span-1 md:inline p-5 border-r border-r-black h-screen">
+            sidebar
+        </div>
+        <div class="p-5 col-span-5 h-screen w-full overflow-scroll">
+        </div>
     </div>
 </body>
 
