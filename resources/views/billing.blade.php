79a3444165b012f9212aa60e4005bf4aae9b132a
livghit <liviu.g01@gmail.com>
2024-06-18 16:20:26 +0200

    wip

--- resources/views/billing.blade.php
+++ resources/views/billing.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 {{-- Here add the laravel way of loading the language  --}}
-<html lang="en">
+<html lang="en"  data-theme="nord">
 
 <head>
     <meta charset="UTF-8">
@@ -9,17 +9,32 @@
     <title>Billing Management</title>
     @livewireScripts
     @livewireStyles
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.2/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <div class="grid grid-cols-6">
-        <div class="hidden md:col-span-1 md:inline p-5 border-r border-r-black h-screen">
-            sidebar
-        </div>
-        <div class="p-5 col-span-5 h-screen w-full overflow-scroll">
-        </div>
-    </div>
+    <div class="drawer">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <label for="my-drawer" class="btn btn-sm btn-primary text-white drawer-button absolute top-5 right-5">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+        </svg>
+    </label>
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+    <p class="text-primary text-3xl my-5">{{config("app.name")}}</p>
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  </div>
+</div>
+    <main>
+
+    </main>
 </body>
 
 </html>
